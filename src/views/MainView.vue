24e7be1fb95944327be9080a9efcedc0fc4a5f26
Jose Luis Pinedo Ceballos <a23311074@uthermosillo.edu.mx>
2025-02-03 17:24:27 -0700

    Se agregaron las funciones de componentes de padres, componentes hijos y props

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,17 +1,23 @@
 <template>
-<div class="container">
-    <Sidebar></Sidebar>
-    <div class="content">
-        <router-view></router-view>
-    </div>
+    <Header :mensaje="texto"></Header>
+    <div class="container">
+        <Sidebar></Sidebar>
+        <div class="content">
+            <input type="text" v-model="texto">
+            {{ texto }}
+            <router-view></router-view>
+        </div>
 
-</div>
+    </div>
 
 </template>
 
 <script setup>
 import Sidebar from '@/components/Sidebar.vue';
-import Sizebar from '@/components/Sidebar.vue';
+import Header from '@/components/Header.vue';
+import { ref } from 'vue';
+const texto = ref('');
+
 </script>
 
 <style>
